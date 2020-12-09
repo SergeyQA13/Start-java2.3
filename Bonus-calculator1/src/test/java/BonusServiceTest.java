5b9e420b39ce7b0c0a529bce72fba798103b746c
Sergey Kolobkov <skolobkov@yandex.ru>
2020-12-09 17:14:12 +0300

    New

--- Bonus-calculator1/src/test/java/BonusServiceTest.java
+++ Bonus-calculator1/src/test/java/BonusServiceTest.java
@@ -6,65 +6,37 @@ class BonusServiceTest {
 
     @Test
     void shouldCalculateRegisteredAndUnderLimit() {
-        BonusService service = new BonusService();
 
+        BonusService service = new BonusService();
         long amount = 1000_60;
         boolean registered = true;
         long expected = 30;
-
         long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
-
     }
 
     @Test
     void shouldCalculateRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
 
+        BonusService service = new BonusService();
         long amount = 1000_000_60;
         boolean registered = true;
-        long expected = 5000;
-
+        long expected = 500;
         long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void shouldCalculateNoRegisteredAndUnderLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = false;
-        long expected = 10;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-
     }
     @Test
-    void shouldCalculateNoRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
+    void shouldCalculateNonRegisteredAndOverLimit() {
 
+        BonusService service = new BonusService();
         long amount = 1000_000_60;
         boolean registered = false;
-        long expected = 3000;
-
+        long expected = 500;
         long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
-
     }
 
-
 }
-
-
-
-
-
-
-
